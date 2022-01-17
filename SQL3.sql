afb848a6f46b7bd9e071c632b05c806248f1485c
KarenVardanian <karen.vardanian@nure.ua>
2022-01-17 17:32:24 +0200

    First

---
+++ SQL3.sql
@@ -0,0 +1,94 @@
+use AdventureWorks2017
+go
+----------------------ЗАДАНИЕ №1-----------------------------
+--Вывести всю информацию из таблицы Sales.Customer
+select*from Sales.Customer
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №2-----------------------------
+--Вывести всю информацию из таблицы Sales.Store отсортированную 
+--по Name в алфавитном порядке
+select *from Sales.Store
+order by Sales.Store.Name asc
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №3-----------------------------
+--Вывести из таблицы HumanResources.Employee всю информацию
+--о десяти сотрудниках, которые родились позже 1989-09-28
+select Top(10)*from HumanResources.Employee
+where BirthDate>'1989-09-28'
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №4-----------------------------
+--Вывести из таблицы HumanResources.Employee сотрудников
+--у которых последний символ LoginID является 0.
+--Вывести только NationalIDNumber, LoginID, JobTitle.
+--Данные должны быть отсортированы по JobTitle по убиванию
+select NationalIDNumber,LoginID,JobTitle
+from HumanResources.Employee
+where HumanResources.Employee.LoginID like'%0'
+order by JobTitle desc
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №5-----------------------------
+--Вывести из таблицы Person.Person всю информацию о записях, которые были 
+--обновлены в 2008 году (ModifiedDate) и MiddleName содержит
+--значение и Title не содержит значение 
+select *from Person.Person
+where ModifiedDate Between '2008'and '2009' and
+Person.MiddleName is not null and
+Title is null
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №6-----------------------------
+--Вывести название отдела (HumanResources.Department.Name) БЕЗ повторений
+--в которых есть сотрудники
+--Использовать таблицы HumanResources.EmployeeDepartmentHistory и HumanResources.Department
+select Distinct HumanResources.Department.Name as hd
+from HumanResources.Department
+inner join HumanResources.EmployeeDepartmentHistory h
+on HumanResources.Department.DepartmentID=h.DepartmentID
+where EndDate is not null
+-------------------------------------------------------------
+go
+
+----------------------ЗАДАНИЕ №7-----------------------------
+--Сгрупировать данные из таблицы Sales.SalesPerson по TerritoryID
+--и вывести сумму CommissionPct, если она больше 0
+select SalesPerson.Bonus, COUNT(CommissionPct)
+from Sales.SalesPerson
+JOIN Sales.SalesTerritory as d
+ON d.TerritoryID=Sales.SalesPerson.TerritoryID
+group by SalesPerson.Bonus
+having COUNT(CommissionPct)>0
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №8-----------------------------
+--Вывести всю информацию о сотрудниках (HumanResources.Employee) 
+--которые имеют самое большое кол-во 
+--отпуска (HumanResources.Employee.VacationHours)
+select Top(1) BusinessEntityID,NationalIDNumber,LoginID,OrganizationNode,OrganizationLevel,JobTitle,
+BirthDate,Max(HumanResources.Employee.VacationHours)
+from HumanResources.Employee
+group by BusinessEntityID,NationalIDNumber,LoginID,OrganizationNode,OrganizationLevel,JobTitle,BirthDate
+
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №9-----------------------------
+--Вывести всю информацию о сотрудниках (HumanResources.Employee) 
+--которые имеют позицию (HumanResources.Employee.JobTitle)
+--'Sales Representative' или 'Network Administrator' или 'Network Manager'
+select*from HumanResources.Employee
+where JobTitle in('Sales Representative','Network Administrator' , 'Network Manager')
+-------------------------------------------------------------
+go
+----------------------ЗАДАНИЕ №10-----------------------------
+--Вывести всю информацию о сотрудниках (HumanResources.Employee) и 
+--их заказах (Purchasing.PurchaseOrderHead).er ЕСЛИ У СОТРУДНИКА НЕТ
+--ЗАКАЗОВ ОН ДОЛЖЕН БЫТЬ ВЫВЕДЕН ТОЖЕ!!!
+select*from HumanResources.Employee  as e
+join Purchasing.PurchaseOrderHeader as p
+on e.BusinessEntityID=p.EmployeeID
+where (p.EmployeeID is not null) or( p.EmployeeID is not null)
+-------------------------------------------------------------
+
